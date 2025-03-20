dae265b91ce3133e17d740b306152c36a88fd158
EEEVA0 <1719123801@qq.com>
2025-03-20 16:41:46 +0800

    delete admin-project1

--- admin-project1/backend/src/main/java/group6/demo/repository/ScooterRepository.java
+++
@@ -1,8 +0,0 @@
-package group6.demo.repository;
-
-import group6.demo.entity.Scooter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScooterRepository extends JpaRepository<Scooter, Long> {
-
-}
--- admin-project1/uniapp-project/unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const _imports_0 = "/static/icons/location.png";
-const _imports_1$1 = "/static/icons/current_location.png";
-const _imports_1 = "/static/icons/arrow-right.png";
-exports._imports_0 = _imports_0;
-exports._imports_1 = _imports_1$1;
-exports._imports_1$1 = _imports_1;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
